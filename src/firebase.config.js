df2327a2d1f0b1a13f113f881364d2d61a1094f4
Tom Chataway <pugtura@gmail.com>
2025-01-27 15:41:19 +1030

    Add firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAt6oOsk4RxmyTHA3DQE5F_lcIRF7FWbKM',
+  authDomain: 'bey-scout.firebaseapp.com',
+  projectId: 'bey-scout',
+  storageBucket: 'bey-scout.firebasestorage.app',
+  messagingSenderId: '779189389844',
+  appId: '1:779189389844:web:7c16d2c24f567f042ef501',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
