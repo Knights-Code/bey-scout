532d472be9eb5be2de3daeb592c0129193d2960e
Tom Chataway <pugtura@gmail.com>
2025-01-28 17:41:27 +1030

    Display reports on map

--- src/functions/fetchReports.js
+++ src/functions/fetchReports.js
@@ -1,6 +1,5 @@
 const fetchReports = async (productOrComponent) => {
   // TODO: Query reports collection in Firebase.
-
   return [
     {
       id: 'blag',
@@ -11,11 +10,26 @@ const fetchReports = async (productOrComponent) => {
       source: {
         name: 'Big W Brickworks',
         geolocation: {
-          lat: -34.914705,
-          lng: 138.565836,
+          lat: -34.913561035265566,
+          lng: 138.56436750560871,
         },
       },
       timestamp: '27 January 2025 at 14:33:53 UTC+10:30',
+    },
+    {
+      id: 'bleg',
+      priceInCents: 1900,
+      product: {
+        name: 'Arrow Wizard 4-80B Starter Pack',
+      },
+      source: {
+        name: 'Target Sefton Park',
+        geolocation: {
+          lat: -34.87685841243745,
+          lng: 138.6030919225819,
+        },
+      },
+      timestamp: '21 January 2025 at 14:33:53 UTC+10:30',
     },
   ]
 }
