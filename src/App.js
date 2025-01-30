42a7fb0ca52507d02afac92dc52cb03f98912aff
Tom Chataway <pugtura@gmail.com>
2025-01-30 15:21:29 +1030

    Remove zoom controls from map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Scout from './pages/Scout'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import ProductForm from './pages/ProductForm'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<Scout />} />
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/new-product-form' element={<ProductForm />} />
         </Routes>
       </Router>
     </>
