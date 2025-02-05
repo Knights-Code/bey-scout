feadd28d1ae390a6ec484c3653f05db42319af94
Tom Chataway <pugtura@gmail.com>
2025-02-05 20:21:38 +1030

    Fix report popup showing midnight for every report

--- src/utilities/countDaysBetween.js
+++ src/utilities/countDaysBetween.js
@@ -1,11 +1,15 @@
 const countDaysBetween = (firstDate, secondDate) => {
+  // Create copies to work with.
+  const firstDateCopy = new Date(firstDate)
+  const secondDateCopy = new Date(secondDate)
+
   // Set times on both dates to midnight to avoid weird rounding.
-  firstDate.setHours(0, 0, 0)
-  secondDate.setHours(0, 0, 0)
+  firstDateCopy.setHours(0, 0, 0)
+  secondDateCopy.setHours(0, 0, 0)
 
   const oneDay = 24 * 60 * 60 * 1000 // hours*minutes*seconds*milliseconds
 
-  return Math.round(Math.abs((firstDate - secondDate) / oneDay))
+  return Math.round(Math.abs((firstDateCopy - secondDateCopy) / oneDay))
 }
 
 export default countDaysBetween
