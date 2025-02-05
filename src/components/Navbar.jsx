75a57b43a6abbb77bbc11f53d044172f6816386e
Tom Chataway <pugtura@gmail.com>
2025-02-05 20:19:09 +1030

    Remove ChakraProvider from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { EditIcon, SearchIcon } from '@chakra-ui/icons'
-import { ChakraProvider } from '@chakra-ui/react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
@@ -13,49 +12,47 @@ const Navbar = () => {
   }
 
   return (
-    <ChakraProvider>
-      <footer className='navbar'>
-        <nav className='navbarNav'>
-          <ul className='navbarListItems'>
-            <li className='navbarListItem' onClick={() => navigate('/')}>
-              <SearchIcon
-                fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
-                width='36px'
-                height='36px'
-              />
-              <p
-                className={
-                  pathMatchRoute('/')
-                    ? 'navbarListItemNameActive'
-                    : 'navbarListItemName'
-                }
-              >
-                Scout
-              </p>
-            </li>
-            <li
-              className='navbarListItem'
-              onClick={() => navigate('/new-report')}
+    <footer className='navbar'>
+      <nav className='navbarNav'>
+        <ul className='navbarListItems'>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
+            <SearchIcon
+              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
             >
-              <EditIcon
-                fill={pathMatchRoute('/new-report') ? '#2c2c2c' : '#8f8f8f'}
-                width='36px'
-                height='36px'
-              />
-              <p
-                className={
-                  pathMatchRoute('/new-report')
-                    ? 'navbarListItemNameActive'
-                    : 'navbarListItemName'
-                }
-              >
-                Report
-              </p>
-            </li>
-          </ul>
-        </nav>
-      </footer>
-    </ChakraProvider>
+              Scout
+            </p>
+          </li>
+          <li
+            className='navbarListItem'
+            onClick={() => navigate('/new-report')}
+          >
+            <EditIcon
+              fill={pathMatchRoute('/new-report') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
+            <p
+              className={
+                pathMatchRoute('/new-report')
+                  ? 'navbarListItemNameActive'
+                  : 'navbarListItemName'
+              }
+            >
+              Report
+            </p>
+          </li>
+        </ul>
+      </nav>
+    </footer>
   )
 }
 
